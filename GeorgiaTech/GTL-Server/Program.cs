497811436918c392d8a3e8feacf5f2afe28cd066
Dennis Rasmussen <denn5682@gmail.com>
2020-03-20 12:50:15 +0100

    started project
    Added GTL - test and GTL - server to project

---
+++ GeorgiaTech/GTL-Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GTL_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ GeorgiaTech/GeorgiaTech/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace GeorgiaTech
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
