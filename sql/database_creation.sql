0e4969a771f8ac72342a1d09977ff8ae48617f02
LindaFuchs <dev@lindafuchs.net>
2020-03-23 14:55:16 +0100

    Refactoring: SQL Reserved word capitalization

--- sql/database_creation.sql
+++ sql/database_creation.sql
@@ -1,227 +1,227 @@
 -- DOCUMENTATION HERE !
 -- IT IS BETTER TO ADD DOCUMENTATION IN THE CODE ITSELF FOR OTHER DEVS ! :)  
-create table [Zip_Code] (
-  zip int not null,
-  city varchar(50) not null,
+CREATE TABLE [Zip_Code] (
+  [zip] INT not null,
+  [city] VARCHAR(50) not null,
 
-  primary key (zip)
+  PRIMARY KEY (zip)
 );
 
-create table [Address] (
-  address_id int not null identity,
-  street varchar(100) not null,
-  additional_info varchar(50),
-  zip int not null,
+CREATE TABLE [Address] (
+  address_id INT not null IDENTITY,
+  street VARCHAR(100) not null,
+  additional_info VARCHAR(50),
+  zip INT not null,
 
-  primary key (address_id),
-  constraint FK_address_zip foreign key (zip)
-    references [Zip_Code] (zip)
-    on update cascade
+  PRIMARY KEY (address_id),
+  CONSTRAINT FK_address_zip FOREIGN KEY (zip)
+    REFERENCES [Zip_Code] (zip)
+    ON UPDATE CASCADE
 );
 
-create table [Member] (
-  member_id int not null identity,
-  ssn varchar(10) not null,
-  f_name varchar(25) not null,
-  l_name varchar(25) not null,
-  home_address_id int not null,
-  campus_address_id int not null,
+CREATE TABLE [Member] (
+  member_id INT not null IDENTITY,
+  ssn VARCHAR(10) not null,
+  f_name VARCHAR(25) not null,
+  l_name VARCHAR(25) not null,
+  home_address_id INT not null,
+  campus_address_id INT not null,
 
-  primary key (member_id),
-  constraint FK_member_home_address_id foreign key (home_address_id)
-    references [Address] (address_id)
-    on update cascade,
-  constraint FK_member_campus_address_id foreign key (campus_address_id)
-    references [Address] (address_id)
-    on update cascade
+  PRIMARY KEY (member_id),
+  CONSTRAINT FK_member_home_address_id FOREIGN KEY (home_address_id)
+    REFERENCES [Address] (address_id)
+    ON UPDATE CASCADE,
+  CONSTRAINT FK_member_campus_address_id FOREIGN KEY (campus_address_id)
+    REFERENCES [Address] (address_id)
+    ON UPDATE CASCADE
 );
 
-create table [Member_Type] (
-  [type_id] int not null identity,
-  [type] varchar(25) not null unique,    
+CREATE TABLE [Member_Type] (
+  [type_id] INT not null IDENTITY,
+  [type] VARCHAR(25) not null unique,    
   
-  primary key ([type_id])
+  PRIMARY KEY ([type_id])
 );
 
-create table [Member_Type_Assignment] (
-  [member_id] int not null,
-  [type_id] int not null,
+CREATE TABLE [Member_Type_Assignment] (
+  [member_id] INT not null,
+  [type_id] INT not null,
 
-    primary key ([member_id],[type_id]),
-    constraint FK_member_type_assignment_member_id foreign key ([member_id])
-        references [Member] ([member_id])
-            on update cascade,
-        constraint FK_member_type_assignment_type_id foreign key ([type_id])
-    references [Member_Type] ([type_id])
-        on update cascade
+    PRIMARY KEY ([member_id],[type_id]),
+    CONSTRAINT FK_member_type_assignment_member_id FOREIGN KEY ([member_id])
+        REFERENCES [Member] ([member_id])
+            ON UPDATE CASCADE,
+        CONSTRAINT FK_member_type_assignment_type_id FOREIGN KEY ([type_id])
+    REFERENCES [Member_Type] ([type_id])
+        ON UPDATE CASCADE
     );
 
 
-create table [Phone_Number] (
-  member_id int not null,
-  phone_number varchar(20) not null,
-  primary key (member_id, phone_number),
+CREATE TABLE [Phone_Number] (
+  member_id INT not null,
+  phone_number VARCHAR(20) not null,
+  PRIMARY KEY (member_id, phone_number),
   
-  constraint FK_phone_number_member_id foreign key (member_id)
-    references [Member] (member_id)
-    on update cascade
-    on delete cascade
+  CONSTRAINT FK_phone_number_member_id FOREIGN KEY (member_id)
+    REFERENCES [Member] (member_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 
-create table [Card] (
-  member_id int not null,
-  photo_path varchar(50) not null,
-  primary key (member_id, photo_path),
+CREATE TABLE [Card] (
+  member_id INT not null,
+  photo_path VARCHAR(50) not null,
+  PRIMARY KEY (member_id, photo_path),
   
-  constraint FK_card_member_id foreign key (member_id)
-    references [Member] (member_id)
-    on update cascade
-    on delete cascade
+  CONSTRAINT FK_card_member_id FOREIGN KEY (member_id)
+    REFERENCES [Member] (member_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 
-create table [Staff] (
-  member_id int not null,
-  job_title varchar(50) not null,
-  primary key (member_id, job_title),
+CREATE TABLE [Staff] (
+  member_id INT not null,
+  job_title VARCHAR(50) not null,
+  PRIMARY KEY (member_id, job_title),
   
-  constraint FK_staff_member_id foreign key (member_id)
-    references [Member] (member_id)
-    on update cascade
-    on delete cascade
+  CONSTRAINT FK_staff_member_id FOREIGN KEY (member_id)
+    REFERENCES [Member] (member_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 
-create table [Author] (
-  author_id int not null identity,
-  first_name varchar(50) not null,
-  last_name varchar(50) not null,
+CREATE TABLE [Author] (
+  author_id INT not null IDENTITY,
+  first_name VARCHAR(50) not null,
+  last_name VARCHAR(50) not null,
 
-  primary key (author_id)
+  PRIMARY KEY (author_id)
 );
 
 
 
-create table [Material] (
-  material_id int not null identity,
-  isbn varchar(100) not null,
-  title varchar(100) not null,
-  [language] varchar(50) not null,
-  lendable bit not null,
-  [type_id] int not null,
-  [description] varchar(max) not null,
+CREATE TABLE [Material] (
+  material_id INT not null IDENTITY,
+  isbn VARCHAR(100) not null,
+  title VARCHAR(100) not null,
+  [language] VARCHAR(50) not null,
+  lendable BIT not null,
+  [type_id] INT not null,
+  [description] VARCHAR(MAX) not null,
 
-  primary key (material_id),
-    constraint FK_material_type_id foreign key ([type_id])
-        references [Material_Type] ([type_id])
-          on update cascade
+  PRIMARY KEY (material_id),
+    CONSTRAINT FK_material_type_id FOREIGN KEY ([type_id])
+        REFERENCES [Material_Type] ([type_id])
+          ON UPDATE CASCADE
 );
 
-create table [Material_Type] (
-    [type_id] int not null identity,
-    [type] varchar(50) not null,
+CREATE TABLE [Material_Type] (
+    [type_id] INT not null IDENTITY,
+    [type] VARCHAR(50) not null,
 
-    primary key ([type_id])
+    PRIMARY KEY ([type_id])
 );
 
-create table [Material_Subject] (
-  material_id int not null,
-  [subject] varchar(50) not null,
+CREATE TABLE [Material_Subject] (
+  material_id INT not null,
+  [subject] VARCHAR(50) not null,
 
-  primary key (material_id, subject),  
-  constraint FK_subject_material_id foreign key (material_id)
-    references [Material] (material_id)
-    on update cascade
-    on delete cascade
+  PRIMARY KEY (material_id, [subject]),  
+  CONSTRAINT FK_subject_material_id FOREIGN KEY (material_id)
+    REFERENCES [Material] (material_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 
-create table [Material_Subject_Assignment] (
-    [material_id] int not null,
-    [subject_id] int not null,
+CREATE TABLE [Material_Subject_Assignment] (
+    [material_id] INT not null,
+    [subject_id] INT not null,
 
-    primary key ([material_id], [subject_id]),
-    constraint FK_material_subject_assignment_material_id foreign key ([material_id])
-        references [Material] ([material_id])
-        on update cascade
-    on delete cascade,
-    constraint FK_material_subject_assignment_subject_id foreign key ([subject_id])
-        references [Material_Subject] ([subject_id])
-        on update cascade
+    PRIMARY KEY ([material_id], [subject_id]),
+    CONSTRAINT FK_material_subject_assignment_material_id FOREIGN KEY ([material_id])
+        REFERENCES [Material] ([material_id])
+        ON UPDATE CASCADE
+    ON DELETE CASCADE,
+    CONSTRAINT FK_material_subject_assignment_subject_id FOREIGN KEY ([subject_id])
+        REFERENCES [Material_Subject] ([subject_id])
+        ON UPDATE CASCADE
 );
 
-create table [Material_Author] (
-  material_id int not null,
-  author_id int not null,
-  primary key (material_id, author_id),
+CREATE TABLE [Material_Author] (
+  material_id INT not null,
+  author_id INT not null,
+  PRIMARY KEY (material_id, author_id),
   
-  constraint FK_material_authors_material_id foreign key (material_id)
-    references [Material] (material_id)
-    on update cascade
-    on delete cascade,
-  constraint FK_material_authors_author_id foreign key (author_id)
-    references [Author] (author_id)
-    on update cascade
-    on delete no action
+  CONSTRAINT FK_material_authors_material_id FOREIGN KEY (material_id)
+    REFERENCES [Material] (material_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  CONSTRAINT FK_material_authors_author_id FOREIGN KEY (author_id)
+    REFERENCES [Author] (author_id)
+    ON UPDATE CASCADE
+    ON DELETE NO ACTION
 );
 
-create table [Volume] (
-  volume_id int not null identity,
-  material_id int not null,
-  home_location_id int not null,
-  current_location_id int not null,
+CREATE TABLE [Volume] (
+  volume_id INT not null IDENTITY,
+  material_id INT not null,
+  home_location_id INT not null,
+  current_location_id INT not null,
   
-  primary key (volume_id),
-  constraint FK_volume_material_id foreign key (material_id)
-    references [Material] (material_id)
-    on update no action
-    on delete no action,
-  constraint FK_volume_home_location_id foreign key (home_location_id)
-    references [Address] (address_id)
-    on update no action
-    on delete no action,
-  constraint FK_volume_current_location_id foreign key (current_location_id)
-    references [Address] (address_id)
-    on update no action
-    on delete no action
+  PRIMARY KEY (volume_id),
+  CONSTRAINT FK_volume_material_id FOREIGN KEY (material_id)
+    REFERENCES [Material] (material_id)
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION,
+  CONSTRAINT FK_volume_home_location_id FOREIGN KEY (home_location_id)
+    REFERENCES [Address] (address_id)
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION,
+  CONSTRAINT FK_volume_current_location_id FOREIGN KEY (current_location_id)
+    REFERENCES [Address] (address_id)
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
 );
 
-create table [Loan] (
-  loan_id int not null identity,
-  member_id int not null,
-  volume_id int not null,
-  loan_date date not null,
-  due_date date not null,
-  extensions int not null,
-  returned_date date,
+CREATE TABLE [Loan] (
+  loan_id INT not null IDENTITY,
+  member_id INT not null,
+  volume_id INT not null,
+  loan_DATE DATE not null,
+  due_DATE DATE not null,
+  extensions INT not null,
+  returned_DATE DATE,
   
-  primary key (loan_id),
-  constraint FK_loan_member_id foreign key (member_id)
-    references [Member] (member_id)
-    on update no action
-    on delete no action,
-  constraint FK_loan_volume_id foreign key (volume_id)
-    references [Volume] (volume_id)
-    on update no action
-    on delete no action
+  PRIMARY KEY (loan_id),
+  CONSTRAINT FK_loan_member_id FOREIGN KEY (member_id)
+    REFERENCES [Member] (member_id)
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION,
+  CONSTRAINT FK_loan_volume_id FOREIGN KEY (volume_id)
+    REFERENCES [Volume] (volume_id)
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
 );
 
 CREATE TABLE [Aquire_Reason](
-    reason_id int not null identity,
-    reason varchar(42),
-    primary key (reason_id),
+    reason_id INT not null IDENTITY,
+    reason VARCHAR(42),
+    PRIMARY KEY (reason_id),
 );
 
 CREATE TABLE [Acquire] (
-  material_id int not null,
-  additional_info varchar(100) not null,
-  reason_id int not null,
+  material_id INT not null,
+  additional_info VARCHAR(100) not null,
+  reason_id INT not null,
 
   PRIMARY KEY (material_id),
-  CONSTRAINT FK_acquire_material_id foreign key (material_id)
-    references [Material] (material_id)
-    on update cascade
-    on delete no action,
-       CONSTRAINT FK_aquire_reason_id foreign key (reason_id)
-        references [Aquire_Reason] (reason_id)
-        on update cascade
+  CONSTRAINT FK_acquire_material_id FOREIGN KEY (material_id)
+    REFERENCES [Material] (material_id)
+    ON UPDATE CASCADE
+    ON DELETE NO ACTION,
+       CONSTRAINT FK_aquire_reason_id FOREIGN KEY (reason_id)
+        REFERENCES [Aquire_Reason] (reason_id)
+        ON UPDATE CASCADE
 
 
 );
