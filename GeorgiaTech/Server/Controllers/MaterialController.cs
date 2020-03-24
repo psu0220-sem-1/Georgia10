59e3299d8d6853a88ecf5d7d505d8ada5299c228
Alexander Ignacz <ignaczalexander@gmail.com>
2020-03-24 18:57:31 +0100

    Add Material controller and interface

---
+++ GeorgiaTech/Server/Controllers/MaterialController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    public class MaterialController: IMaterialController
+    {
+        public MaterialController()
+        {
+        }
+
+        public int Delete(Material t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Material> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Material FindByID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Material FindByType(Material t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Material Insert(Material t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Material Update(Material t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
