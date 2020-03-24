f4a94b82aba20684754f16e4a2c1d5afbd32baf5
Alexander Ignacz <ignaczalexander@gmail.com>
2020-03-24 20:41:20 +0100

    Implement Create in VolumeController

---
+++ GeorgiaTech/Api/Controllers/VolumeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+using Server.Controllers;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    public class VolumeController : Controller
+    {
+        private readonly Server.Controllers.VolumeController volumeController;
+        public VolumeController(GTLContext context)
+        {
+            volumeController = new Server.Controllers.VolumeController(context);
+        }
+        // GET: api/values
+        [HttpGet]
+        public List<Volume> Get()
+        {
+            var volumes = volumeController.FindAll();
+            return volumes;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
