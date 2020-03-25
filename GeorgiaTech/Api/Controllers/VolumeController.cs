79825fc98212c41e72954058c6001b717ab3b51d
Alexander Ignacz <ignaczalexander@gmail.com>
2020-03-25 21:30:10 +0100

    Add builder function

--- GeorgiaTech/Api/Controllers/VolumeController.cs
+++ GeorgiaTech/Api/Controllers/VolumeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Server.Models;
+using Api.Models;
 using Server.Controllers;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,10 +20,64 @@ namespace Api.Controllers
         }
         // GET: api/values
         [HttpGet]
-        public List<Volume> Get()
+        public List<Models.Volume> Get()
         {
             var volumes = volumeController.FindAll();
-            return volumes;
+            var models = volumes.Select(volume => BuildVolume(volume)).ToList();
+
+            return models;
+
+        }
+
+        public Models.Volume BuildVolume(Server.Models.Volume volume)
+        {
+
+            var homeAddress = new Models.Address
+            {
+                AddressId = volume.HomeLocation.AddressId,
+                Street = volume.HomeLocation.Street,
+                AdditionalInfo = volume.HomeLocation.AdditionalInfo,
+                Zip = volume.HomeLocation.Zip,
+                City = volume.HomeLocation.ZipNavigation.City
+            };
+
+            var currentAddress = new Models.Address
+            {
+                AddressId = volume.CurrentLocation.AddressId,
+                Street = volume.CurrentLocation.Street,
+                AdditionalInfo = volume.CurrentLocation.AdditionalInfo,
+                Zip = volume.CurrentLocation.Zip,
+                City = volume.CurrentLocation.ZipNavigation.City
+            };
+
+            var volumeMaterial = new Models.Material
+            {
+                MaterialId = volume.MaterialId,
+                Isbn = volume.Material.Isbn,
+                Language = volume.Material.Language,
+                Lendable = volume.Material.Lendable,
+                Description = volume.Material.Description,
+                Authors = volume.Material.MaterialAuthor.Select(e => new Models.Author
+                {
+                    AuthorId = e.AuthorId,
+                    FirstName = e.Author.FirstName,
+                    LastName = e.Author.LastName
+                })
+                .ToList(),
+                MaterialSubjects = volume.Material.MaterialSubjectAssignment.Select(e => new Models.MaterialSubject
+                {
+                    SubjectId = e.SubjectId,
+                    Subject = e.Subject.Subject
+                }).ToList()
+            };
+            var modelVolume = new Models.Volume
+            {
+                VolumeId = volume.VolumeId,
+                HomeLocation = homeAddress,
+                CurrentLocation = currentAddress,
+                Material = volumeMaterial
+            };
+            return modelVolume;
         }
 
         // GET api/values/5
