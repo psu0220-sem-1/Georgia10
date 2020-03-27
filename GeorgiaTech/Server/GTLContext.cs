79dea6dcc2e137e5d4db895a96185de4eaba4c60
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-03-27 10:47:23 +0100

    Update context namespace

--- GeorgiaTech/Server/GTLContext.cs
+++ GeorgiaTech/Server/GTLContext.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Models;
 
-namespace Server.Models
+namespace Server
 {
     public partial class GTLContext : DbContext
     {
