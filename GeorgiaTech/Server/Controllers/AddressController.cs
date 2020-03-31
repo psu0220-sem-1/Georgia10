06067e45d47ce8a52139043d2202b6cdf378f0ea
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-03-31 13:40:20 +0200

    Implement AddressController Create method
    Implement the method and tests for it. Documentation included.

--- GeorgiaTech/Server/Controllers/AddressController.cs
+++ GeorgiaTech/Server/Controllers/AddressController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Server.Models;
 
 namespace Server.Controllers
@@ -42,9 +44,39 @@ namespace Server.Controllers
             throw new System.NotImplementedException();
         }
 
-        public Address Create(string street, string additionalInfo, int zip)
+        /// <summary>
+        /// Creates an address instance based on the information passed.
+        /// The zipCode will be found in the database and the object will be assigned to
+        /// the Zip property of the entity. If such a zip code doesn't exist in the database
+        /// the method will throw an ArgumentException
+        /// </summary>
+        /// <param name="street">The street of the address, including the street number</param>
+        /// <param name="additionalInfo">The floor and apartment of the address</param>
+        /// <param name="zipCode">The zip code as an integer</param>
+        /// <returns>An address entity including its Zip navigational property</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public Address Create(string street, string additionalInfo, int zipCode)
         {
-            throw new System.NotImplementedException();
+            if (street.Length == 0)
+                throw new ArgumentOutOfRangeException(nameof(street), "Street can't be empty");
+
+            if (additionalInfo.Length == 0)
+                throw new ArgumentOutOfRangeException(nameof(additionalInfo), "Additional info can't be empty");
+
+            var zip = _db.ZipCodes.Find(zipCode);
+
+            if (zip == null)
+                throw new ArgumentException("Zip code provided must already exist in the database", nameof(zipCode));
+
+            var address = new Address
+            {
+                Street = street,
+                AdditionalInfo = additionalInfo,
+                Zip = zip,
+            };
+
+            return address;
         }
     }
 }
