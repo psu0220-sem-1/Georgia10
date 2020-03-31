ebc37a16dadc5a2bb829d5ef10d225dcc3c59080
Alexander Ignacz <ignaczalexander@gmail.com>
2020-03-31 15:20:55 +0200

    Update server volume controller to use exceptions

--- GeorgiaTech/Server/Controllers/VolumeController.cs
+++ GeorgiaTech/Server/Controllers/VolumeController.cs
@@ -22,13 +22,23 @@ namespace Server.Controllers
 
         public Volume Create(int materialID, int homeLocationID, int currentLocationID)
         {
-            var material = materialController.FindByID(materialID);
-            var homeLocation = addressController.FindByID(homeLocationID);
-            var currentLocation = addressController.FindByID(currentLocationID);
+     
+            try
+            {
+                var material = materialController.FindByID(materialID);
+                var homeLocation = addressController.FindByID(homeLocationID);
+                var currentLocation = addressController.FindByID(currentLocationID);
 
-            var volume = new Volume { Material = material, CurrentLocation = currentLocation, HomeLocation = homeLocation };
+                var volume = new Volume { Material = material, CurrentLocation = currentLocation, HomeLocation = homeLocation };
+                Insert(volume);
+                return FindByID(ID: volume.VolumeId);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
 
-            return volume;
+            }
+        
         }
 
         public int Delete(Volume t)
@@ -53,22 +63,55 @@ namespace Server.Controllers
             return volumes.ToList();
         }
 
+        public List<Volume> FindVolumesForMaterial(int materialId)
+        {
+            try
+            {
+                IQueryable<Volume> volumes = _context.Volumes.Where(v => v.MaterialId == materialId)
+              .Include(v => v.Material)
+                  .ThenInclude(m => m.MaterialAuthors)
+                      .ThenInclude(ma => ma.Author)
+              .Include(v => v.Material)
+                  .ThenInclude(m => m.MaterialSubjects)
+                      .ThenInclude(ms => ms.MaterialSubject)
+              .Include(v => v.HomeLocation)
+                  .ThenInclude(a => a.Zip)
+              .Include(v => v.CurrentLocation)
+                  .ThenInclude(a => a.Zip);
+
+                return volumes.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+          
+        }
+
         public Volume FindByID(int ID)
         {
-            var volume = _context.Volumes
-                .Include(v => v.Material)
-                    .ThenInclude(m => m.MaterialAuthors)
-                        .ThenInclude(ma => ma.Author)
-                .Include(v => v.Material)
-                    .ThenInclude(m => m.MaterialSubjects)
-                        .ThenInclude(ms => ms.MaterialSubject)
-                .Include(v => v.HomeLocation)
-                    .ThenInclude(a => a.Zip)
-                .Include(v => v.CurrentLocation)
-                    .ThenInclude(a => a.Zip)
-                 .Single(v => v.VolumeId == ID);
+            try
+            {
+                var volume = _context.Volumes
+        .Include(v => v.Material)
+            .ThenInclude(m => m.MaterialAuthors)
+                .ThenInclude(ma => ma.Author)
+        .Include(v => v.Material)
+            .ThenInclude(m => m.MaterialSubjects)
+            .ThenInclude(ms => ms.MaterialSubject)
+        .Include(v => v.HomeLocation)
+            .ThenInclude(a => a.Zip)
+         .Include(v => v.CurrentLocation)
+             .ThenInclude(a => a.Zip)
+         .Single(v => v.VolumeId == ID);
 
-            return volume;
+                return volume;
+            }
+            catch
+            {
+                throw new NullReferenceException(message: $"Volume with id: {ID} not found");
+            }
+    
         }
 
         public Volume FindByType(Volume t)
