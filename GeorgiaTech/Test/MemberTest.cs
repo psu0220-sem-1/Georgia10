277aa594c608a6905013e046d1ccf566e473a61e
Dennis Rasmussen <denn5682@gmail.com>
2020-04-07 18:57:05 +0200

    made setup of tests, prepared environment for testing.

--- GeorgiaTech/Test/MemberTest.cs
+++ GeorgiaTech/Test/MemberTest.cs
@@ -5,24 +5,56 @@ using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using Server;
 using Server.Controllers;
+using Server.Models;
 
 namespace Test
 {
     [TestFixture]
     public class MemberTest
     {
+        string fName = "";
+        string lName = "";
+        string ssn = "250-57-9705";
+        string homeAddres = "Annemosevej";
+        string campusAddress = "Sofiendalsvej 60";
+        string homeAddressAdditionalInfo = "";
+        int zip = 8858;
+        List<MemberType> mTypes = new List<MemberType>();
+        //To avoid setting these things up each time.
         
         [SetUp]
         public void Setup()
         {
             //gonna need to assign dbContext somewhere / somehow.
-            GTLContext dbContext;
+            //MemberTypes:
+            MemberType student = new MemberType()
+            {
+                TypeId = 0,
+                TypeName = "Student"
+            };
+            MemberType staff = new MemberType()
+            {
+                TypeId = 1,
+                TypeName = "Staff"
+            };
+            MemberType faculty = new MemberType()
+            {
+                TypeId = 2,
+                TypeName = "Faculty"
+            };
+            List<MemberType> memberTypes = new List<MemberType>();
+            mTypes.Add(student);
+            mTypes.Add(staff);
+            mTypes.Add(faculty);
             // setup
             var options = new DbContextOptionsBuilder<GTLContext>()
-                .UseInMemoryDatabase("InsertThrowsArgumentNullExceptionWithNullAuthorParam")
+                .UseInMemoryDatabase("MemberTestDatabase")
                 .Options;
-            
-            IMemberController membercontroller = ControllerFactory.CreateMemberController(dbContext);
+            using (var context = new GTLContext(options))
+            {
+                IMemberController membercontroller = ControllerFactory.CreateMemberController(context);
+                var member = membercontroller.Create(ssn, fName, lName, homeAddres, campusAddress, zip, homeAddressAdditionalInfo, mTypes);
+            }
         }
 
         [Test]
