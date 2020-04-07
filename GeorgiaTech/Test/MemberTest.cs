b78a7d8a6c71c7b3dce41cd5eb11626f69beda49
Dennis Rasmussen <denn5682@gmail.com>
2020-04-07 13:20:37 +0200

    started work on tests of Controller functions

---
+++ GeorgiaTech/Test/MemberTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Server;
+using Server.Controllers;
+
+namespace Test
+{
+    [TestFixture]
+    public class MemberTest
+    {
+        
+        [SetUp]
+        public void Setup()
+        {
+            //gonna need to assign dbContext somewhere / somehow.
+            GTLContext dbContext;
+            // setup
+            var options = new DbContextOptionsBuilder<GTLContext>()
+                .UseInMemoryDatabase("InsertThrowsArgumentNullExceptionWithNullAuthorParam")
+                .Options;
+            
+            IMemberController membercontroller = ControllerFactory.CreateMemberController(dbContext);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
