d33fb5245fb40c92c91e1d637a4d4490d37d5efc
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-04-23 11:30:16 +0200

    Implement GET, POST, PUT and DELETE methods

--- GeorgiaTech/Api/Controllers/AuthorController.cs
+++ GeorgiaTech/Api/Controllers/AuthorController.cs
@@ -1,4 +1,5 @@
-using System.Text.Json;
+using System;
+using Castle.Core.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Server;
 using Server.Controllers;
@@ -7,27 +8,86 @@ using Server.Models;
 namespace Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/author")]
     public class AuthorController: ControllerBase
     {
         private readonly IAuthorController _controller;
 
         public AuthorController(GTLContext context)
         {
-            _controller = new Server.Controllers.AuthorController(context);
+            _controller = ControllerFactory.CreateAuthorController(context);
+        }
+
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            var authors = _controller.FindAll();
+            return new JsonResult(authors);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetAuthor(int id)
+        {
+            var author = _controller.FindByID(id);
+
+            if (author == null)
+                return new NotFoundResult();
+
+            return new JsonResult(author);
         }
 
         [HttpPost]
-        public Author Post([FromBody] JsonElement content)
+        public IActionResult CreateAuthor([FromBody] Author authorData)
         {
-            var firstName = content.GetProperty("firstName").GetString();
-            var lastName = content.GetProperty("lastName").GetString();
+            if (authorData.FirstName.IsNullOrEmpty() || authorData.LastName.IsNullOrEmpty())
+                return new BadRequestResult();
 
+            var author = _controller.Create(authorData.FirstName, authorData.LastName);
+            _controller.Insert(author);
 
-            var author = _controller.Create(firstName, lastName);
-            author = _controller.Insert(author);
+            var uri = new Uri($"http://{Request.Host}/api/author/{author.AuthorId}");
 
-            return author;
+            return new CreatedResult(uri, author);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateAuthor(int id, [FromBody] Author authorData)
+        {
+            var author = _controller.FindByID(id);
+            if (author == null)
+                return new NotFoundResult();
+
+            if (!authorData.FirstName.IsNullOrEmpty())
+                author.FirstName = authorData.FirstName;
+
+            if (!authorData.LastName.IsNullOrEmpty())
+                author.LastName = authorData.LastName;
+
+            var rowsChanged = _controller.Update(author);
+            if (rowsChanged != 1)
+            {
+                // log error, maybe even stop the program as it's in an inconsistent state?
+                return new StatusCodeResult(500);
+            }
+
+            return new JsonResult(author);
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteAuthor(int id)
+        {
+            var author = _controller.FindByID(id);
+            if (author == null)
+                return new NotFoundResult();
+
+            var rowsChanged = _controller.Delete(author);
+            if (rowsChanged != 1)
+            {
+                // log error, maybe even stop the program as it's in an inconsistent state?
+                return new StatusCodeResult(500);
+            }
+
+            return new OkResult();
         }
     }
 }
