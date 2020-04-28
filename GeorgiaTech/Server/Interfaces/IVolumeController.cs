1e2b2187777ee5ff7afdf233b0097837d8497261
Alexander Ignacz <ignaczalexander@gmail.com>
2020-04-28 13:43:44 +0200

    Add FindVolumesForMaterial method to IVolumeController

--- GeorgiaTech/Server/Interfaces/IVolumeController.cs
+++ GeorgiaTech/Server/Interfaces/IVolumeController.cs
@@ -6,5 +6,6 @@ namespace Server.Controllers
     public interface IVolumeController: IController<Volume>
     {
         public Volume Create(int materialId, int homeLocationId, int currentLocationId);
+        public List<Volume> FindVolumesForMaterial(int materialId);
     }
 }
