1a34e412d1241059cc2ba32da328e285463727ae
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-04-29 14:33:56 +0200

    Implement BuildMaterial helper method and basic GETs

--- GeorgiaTech/Api/Controllers/MaterialController.cs
+++ GeorgiaTech/Api/Controllers/MaterialController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Api.Models;
 using Microsoft.AspNetCore.Mvc;
 using Server;
@@ -17,16 +18,50 @@ namespace Api.Controllers
             _controller = ControllerFactory.CreateMaterialController(context);
         }
 
+        private Api.Models.Material BuildMaterial(Server.Models.Material material)
+        {
+            var authors = material.MaterialAuthors
+                .Select(ma => new Api.Models.Author
+                {
+                    AuthorId = ma.Author.AuthorId,
+                    FirstName = ma.Author.FirstName,
+                    LastName = ma.Author.LastName,
+                })
+                .ToList();
+
+            var subjects = material.MaterialSubjects
+                .Select(ms => new Api.Models.MaterialSubject
+                {
+                    SubjectId = ms.MaterialSubject.SubjectId,
+                    Subject = ms.MaterialSubject.SubjectName,
+                })
+                .ToList();
+
+            return new Api.Models.Material
+            {
+                MaterialId = material.MaterialId,
+                Isbn = material.Isbn,
+                Language =  material.Language,
+                Description = material.Description,
+                Type = material.Type,
+                Authors = authors,
+                MaterialSubjects = subjects,
+            };
+        }
+
         [HttpGet]
         public IActionResult GetAll()
         {
-            throw new NotImplementedException();
+            var materials = _controller.FindAll();
+
+            return new JsonResult(materials.Select(BuildMaterial).ToList());
         }
 
         [HttpGet("{id:int}")]
         public IActionResult GetById(int id)
         {
-            throw new NotImplementedException();
+            var material = _controller.FindByID(id);
+            return new JsonResult(BuildMaterial(material));
         }
 
         [HttpPost]
