95404525e13304a7f9eb5ff72c6c6d826ee87754
Alexander Ignacz <ignaczalexander@gmail.com>
2020-04-29 21:09:08 +0200

    Add required field to model

--- GeorgiaTech/Api/Models/Address.cs
+++ GeorgiaTech/Api/Models/Address.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Api.Models
 {
     public class Address
     {
         public int AddressId { get; set; }
+        [Required]
         public string Street { get; set; }
+        [Required]
         public string AdditionalInfo { get; set; }
+        [Required]
         public int Zip { get; set; }
         public string City { get; set; }
     }
