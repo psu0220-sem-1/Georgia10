5b5a675c9daa8493cd96d2e64fedbdedf99a3dc3
Alexander Ignacz <ignaczalexander@gmail.com>
2020-04-30 09:54:38 +0200

    Remove required annotation

--- GeorgiaTech/Api/Models/Address.cs
+++ GeorgiaTech/Api/Models/Address.cs
@@ -6,11 +6,8 @@ namespace Api.Models
     public class Address
     {
         public int AddressId { get; set; }
-        [Required]
         public string Street { get; set; }
-        [Required]
         public string AdditionalInfo { get; set; }
-        [Required]
         public int Zip { get; set; }
         public string City { get; set; }
     }
