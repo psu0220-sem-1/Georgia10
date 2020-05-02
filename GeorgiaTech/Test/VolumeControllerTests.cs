b5b48db10bed7df5877f6183c7bd6ece5a8d76a9
Alexander Ignacz <ignaczalexander@gmail.com>
2020-05-02 13:50:10 +0200

    Add test to check volume deletion

--- GeorgiaTech/Test/VolumeControllerTests.cs
+++ GeorgiaTech/Test/VolumeControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
@@ -12,10 +13,32 @@ namespace Test
     [TestFixture]
     public class VolumeControllerTests
     {
+        private List<Volume> volumes;
+
         [SetUp]
         public void Setup()
         {
-
+            volumes = new List<Volume>
+            {
+                new Volume
+                {
+                    MaterialId = 1,
+                    CurrentLocationId =1,
+                    HomeLocationId =2
+                },
+                  new Volume
+                {
+                    MaterialId = 2,
+                    CurrentLocationId =1,
+                    HomeLocationId =2
+                },
+                    new Volume
+                {
+                    MaterialId = 3,
+                    CurrentLocationId = 1,
+                    HomeLocationId = 2
+                },
+            };
         }
 
         [Test]
@@ -29,7 +52,7 @@ namespace Test
             using (var context = new GTLContext(options))
             {
                 var volumeController = ControllerFactory.CreateVolumeController(context);
-                var volume = new Volume { MaterialId = materialId, HomeLocationId = homeLocationId, CurrentLocationId = currentLocationId};
+                var volume = new Volume { MaterialId = materialId, HomeLocationId = homeLocationId, CurrentLocationId = currentLocationId };
                 volumeController.Insert(volume);
             }
 
@@ -54,11 +77,11 @@ namespace Test
         {
             var methodName = MethodBase.GetCurrentMethod().Name;
             var options = new DbContextOptionsBuilder<GTLContext>().UseInMemoryDatabase(methodName).Options;
-            
+
             var materialId = 1;
             var homeLocationId = 1;
             var currentLocationId = 1;
-            
+
             using (var context = new GTLContext(options))
             {
                 var volumeController = ControllerFactory.CreateVolumeController(context);
@@ -67,6 +90,40 @@ namespace Test
 
             }
 
+        }
+
+        [Test]
+        public void DeleteDeletesCorrectly()
+        {
+            var methodName = MethodBase.GetCurrentMethod().Name;
+            var options = new DbContextOptionsBuilder<GTLContext>().UseInMemoryDatabase(methodName).Options;
+
+            // add volumes to db
+            using (var context = new GTLContext(options))
+            {
+                context.AddRange(volumes);
+                context.SaveChanges();
+            }
+
+            // delete volume
+            using (var context = new GTLContext(options))
+            {
+
+                var volumeController = ControllerFactory.CreateVolumeController(context);
+
+                var volumeToDelete = volumes[1];
+
+                volumeController.Delete(volumeToDelete);
+
+                var fetchedVolume = context.Volumes.Find(volumeToDelete.VolumeId);
+                var fetchedVolumes = context.Volumes.ToList();
+
+                // assert that the fetched volume is null - deleted from db
+                Assert.That(fetchedVolume, Is.Null);
+                // assert that number of volumes decreased by 1
+                Assert.AreEqual(fetchedVolumes.Count, volumes.Count - 1);
+
+            }
         }
     }
 }
