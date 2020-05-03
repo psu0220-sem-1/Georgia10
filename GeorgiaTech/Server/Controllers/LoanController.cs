22261f8601ad1614c693dc116dd0a9d5fff3ce58
Alexander Ignacz <ignaczalexander@gmail.com>
2020-05-03 16:38:47 +0200

    Create LoanController class, no methods are implemented

---
+++ GeorgiaTech/Server/Controllers/LoanController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    public class LoanController: ILoanController
+    {
+        private readonly GTLContext _context;
+
+        public LoanController(GTLContext context)
+        {
+            _context = context;
+        }
+
+        public int Delete(Loan t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Loan> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Loan FindByID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Loan t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Loan t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
