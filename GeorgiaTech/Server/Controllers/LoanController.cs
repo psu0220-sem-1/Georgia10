420a23fbbff9a61980cf3fd574e15bce53d00552
Nikola Anastasov Velichkov <federlizer@gmail.com>
2020-05-06 13:25:26 +0200

    Implement Create method

--- GeorgiaTech/Server/Controllers/LoanController.cs
+++ GeorgiaTech/Server/Controllers/LoanController.cs
@@ -9,11 +9,45 @@ namespace Server.Controllers
 {
     public class LoanController : ILoanController
     {
+        // in days
+        public const int LendingPeriod = 21;
+        public const int GracePeriod = 7;
+        public const int ProfessorLendingPeriod = 90;
+        public const int ProfessorGracePeriod = 14;
+
         private readonly GTLContext _context;
 
         public LoanController(GTLContext context)
         {
             _context = context;
+        }
+
+        public Loan Create(Member member, Volume volume)
+        {
+            var today = DateTime.Today;
+            var dueDate = today;
+
+            foreach (var membership in member.Memberships)
+            {
+                if (membership.MemberType.TypeName == "Professor")
+                    dueDate = today.AddDays(ProfessorLendingPeriod);
+            }
+
+            if (dueDate == today)
+                dueDate = today.AddDays(LendingPeriod);
+
+
+            var loan = new Loan
+            {
+                Member = member,
+                Volume = volume,
+                LoanDate = today,
+                DueDate = dueDate,
+                ReturnedDate = null,
+                Extensions = 0,
+            };
+
+            return loan;
         }
 
         public int Delete(Loan t)
--- GeorgiaTech/Server/Interfaces/ILoanController.cs
+++ GeorgiaTech/Server/Interfaces/ILoanController.cs
@@ -5,6 +5,7 @@ namespace Server.Controllers
 {
     public interface ILoanController: IController<Loan>
     {
+        public Loan Create(Member member, Volume volume);
         public List<Loan> FindForMember(int memberId);
     }
 }
